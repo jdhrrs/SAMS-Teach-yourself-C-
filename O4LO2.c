b9dce6d37f9cc8129537ca4616896923fd782909
jdhrrs <51782837+jdhrrs@users.noreply.github.com>
2020-07-14 20:44:11 -0500

    Add files via upload

---
+++ O4LO2.c
@@ -0,0 +1,15 @@
+/* O4LO2.c: Converting numeric values back to characters */
+
+#include<stdio.h>
+
+int main()
+{
+	char c1;
+	char c2;
+
+	c1 =65;
+	c2 =97;
+	printf("The character that has the numeric value of 65 is: %c.\n", c1);
+	printf("The character that has the numeric value of 97 is: %c.\n", c2);
+	return 0;
+}
